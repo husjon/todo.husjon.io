89d0ca31fd07bee0e4e4348bef7cb89951ab50e5
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-11 21:04:59 +0200

    updated shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell rec {
+pkgs.mkShell {
   packages = with pkgs; [
     # your packages here (e.g: nodePackages_latest.pnpm)
     nodePackages_latest.pnpm
+    nodejs_22
   ];
 
   shellHook = ''

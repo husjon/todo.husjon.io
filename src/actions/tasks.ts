d4bc85f8ee0afc28c200d1b137499d5bf80d8b6c
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-16 21:13:51 +0200

    updated createTask

--- src/actions/tasks.ts
+++ src/actions/tasks.ts
@@ -1,8 +1,19 @@
 "use server";
 
-import { Task } from "../drizzle/schema/task";
+import { revalidatePath } from "next/cache";
+import { db } from "../drizzle/db";
+import { Task, TaskTable } from "../drizzle/schema/task";
 
-export async function createTask(task: string) {}
+export async function createTask(formData: FormData) {
+  const task = formData.get("task") as string;
+  if (!task) return;
+
+  await db.insert(TaskTable).values({
+    task,
+  });
+
+  revalidatePath("/");
+}
 
 export async function getTasks() {
   return [

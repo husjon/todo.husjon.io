db2457200c785042150c5518ec01ac7bf2cbdaa3
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-17 23:18:09 +0200

    replaced postgres.js with node-postgres

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,10 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { TaskTable } from "./schema/task";
 
-export const connection = postgres(process.env.DATABASE_URL as string);
+export const connection = new Pool({
+  connectionString: process.env.DATABASE_URL as string,
+});
 
 export const db = drizzle(connection, {
   // logger: true,

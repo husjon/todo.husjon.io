27ddd9d4fd92653abfe88832cfb47f7f57428949
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-17 23:26:26 +0200

    updated migrate script with limited connection count

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
 
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { db, connection } from "./db";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
 async function doMigration() {
+  const connection = new Pool({
+    connectionString: process.env.DATABASE_URL as string,
+    max: 1,
+  });
+
+  const db = drizzle(connection);
+
   await migrate(db, { migrationsFolder: "./src/drizzle/migrations" });
+
   await connection.end();
 }
 
